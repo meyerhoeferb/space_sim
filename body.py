f1b18557c034cba0b87986ce2bdb01650e04ed61
Braden Meyerhoefer <bredcurve@gmail.com>
2024-08-01 17:58:18 -0400

    Add bodies orbiting with orbital path tail

--- body.py
+++ body.py
@@ -1,5 +1,8 @@
-from typing import Tuple
+from __future__ import annotations
+
+from typing import Tuple, List
 import pygame
+import math
 
 
 class Body:
@@ -13,6 +16,8 @@ class Body:
         radius: float,
         color: Tuple[int],
         mass: float,
+        i_vx: float = 0,
+        i_vy: float = 0,
         is_sun: bool = False,
     ) -> None:
         self.x = x
@@ -25,12 +30,71 @@ class Body:
         self.sun = is_sun  # is this the sun
         self.distance_to_sun = 0  # in AU
 
-        self.x_vel = 0
-        self.y_vel = 0
+        self.x_vel = i_vx  # in m/s
+        self.y_vel = i_vy
 
     def draw(self, win: pygame.Surface, scale: float, w: int, h: int):
         # scale the current position and center on window
         x = self.x * scale + w / 2
         y = self.y * scale + h / 2
 
+        # draw orbital path
+        if len(self.orbit) >= 2:
+            to_draw_orbit = []
+            for point in self.orbit:
+                x, y = point
+                x = x * scale + w / 2
+                y = y * scale + h / 2
+                to_draw_orbit.append((x, y))
+
+            pygame.draw.lines(win, self.color, False, to_draw_orbit, 2)
+
         pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+    def attraction(self, other: Body) -> Tuple[float, float]:
+        """calculate the force of attraction in x and y direction between this body and some other body
+
+        Args:
+            other (Body): body to calculate force to
+
+        Returns:
+            Tuple[float, float]: force in x dir, force in y dir
+        """
+        # calculate distance between objects
+        distance_x = other.x - self.x
+        distance_y = other.y - self.y
+
+        distance = math.sqrt(distance_x**2 + distance_y**2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+
+        # calculate force between objects in x and y direction
+        force = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+
+        return force_x, force_y
+
+    def update_position(self, bodies: List[Body], timestep: int):
+        total_fx = total_fy = 0
+        for b in bodies:
+            if self == b:
+                continue
+
+            fx, fy = self.attraction(b)
+
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * timestep
+        self.y_vel += total_fy / self.mass * timestep
+
+        self.x += self.x_vel * timestep
+        self.y += self.y_vel * timestep
+
+        # keep it at just last n positions
+        if len(self.orbit) > 50:
+            self.orbit.pop(0)
+        self.orbit.append((self.x, self.y))
--- solar_system.py
+++ solar_system.py
@@ -12,6 +12,7 @@ pygame.display.set_caption("The Solar System")
 SCALE = 150 / Body.AU  # at 250, 1au = 100 pixels
 TIMESTEP = 3600 * 24  # 1 day in seconds
 
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149, 237)
@@ -25,15 +26,17 @@ def main():
 
     # initialize our bodies
     sun = Body(0, 0, 30, YELLOW, 1.98892 * 10**30, True)
-    earth = Body(-1 * Body.AU, 0, 16, BLUE, 5.9742 * 10**24)
-    mars = Body(-1.524 * Body.AU, 0, 12, RED, 6.39 * 10**23)
-    mercury = Body(0.387 * Body.AU, 0, 8, DARK_GREY, 3.3 * 10**23)
-    venus = Body(0.723 * Body.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    earth = Body(-1 * Body.AU, 0, 16, BLUE, 5.9742 * 10**24, i_vy=29.783 * 1000)
+    mars = Body(-1.524 * Body.AU, 0, 12, RED, 6.39 * 10**23, i_vy=24.077 * 1000)
+    mercury = Body(0.387 * Body.AU, 0, 8, DARK_GREY, 3.3 * 10**23, i_vy=-47.4 * 1000)
+    venus = Body(0.723 * Body.AU, 0, 14, WHITE, 4.8685 * 10**24, i_vy=-35.02 * 1000)
 
     bodies = [sun, earth, mars, mercury, venus]
 
     while run:
         clock.tick(60)  # loop will run at most 60 times a second
+        WIN.fill(BLACK)  # have to draw over last stuff each time they update
+
         # handle all events
         for event in pygame.event.get():
             # handle someone exiting the window
@@ -42,6 +45,7 @@ def main():
 
         # render position of bodies
         for body in bodies:
+            body.update_position(bodies, TIMESTEP)
             body.draw(WIN, SCALE, WIDTH, HEIGHT)
 
         # update screen to show changes
